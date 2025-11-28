bbe8033993d05d0f91276719deb54ed4bb075a86
한진희 <0200134hjh@gmail.com>
2025-11-28 16:54:23 +0900

    Update modernism_ir.rs

--- src/modernism_ir.rs
+++ src/modernism_ir.rs
@@ -1,6 +1,28 @@
+use crate::ast::Node;
+
 #[derive(Debug, Clone)]
 pub enum ModernIR {
     Module(String),
     Print(String),
     Exit,
+}
+
+pub fn ast_to_modernism_ir(ast: Vec<Node>) -> Vec<ModernIR> {
+    let mut out = Vec::new();
+
+    for node in ast {
+        match node {
+            Node::ProgramId(name) => {
+                out.push(ModernIR::Module(name));
+            }
+            Node::Display(msg) => {
+                out.push(ModernIR::Print(msg));
+            }
+            Node::StopRun => {
+                out.push(ModernIR::Exit);
+            }
+        }
+    }
+
+    out
 }
