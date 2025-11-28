8e1974a5f7c6ce9a1f887f7e24f9e9e1def2c799
한진희 <0200134hjh@gmail.com>
2025-11-28 16:54:48 +0900

    Update spongelang_gen.rs

--- src/spongelang_gen.rs
+++ src/spongelang_gen.rs
@@ -1,19 +1,39 @@
 use crate::modernism_ir::ModernIR;
-use crate::spongelang_ir::SpongeIR;
+
+#[derive(Debug, Clone)]
+pub enum SpongeIR {
+    Block(String),
+    Echo(String),
+    Dissolve,
+}
 
 pub fn modernism_to_spongelang(ir: Vec<ModernIR>) -> Vec<SpongeIR> {
-    let mut out = vec![];
+    let mut v = Vec::new();
 
     for item in ir {
         match item {
-            ModernIR::Module(name) => {
-                out.push(SpongeIR::Block(name));
-            },
-            ModernIR::Print(msg) => {
-                out.push(SpongeIR::Echo(msg));
-            },
-            ModernIR::Exit => {
-                out.push(SpongeIR::Dissolve);
+            ModernIR::Module(name) => v.push(SpongeIR::Block(name)),
+            ModernIR::Print(msg) => v.push(SpongeIR::Echo(msg)),
+            ModernIR::Exit => v.push(SpongeIR::Dissolve),
+        }
+    }
+
+    v
+}
+
+pub fn generate_spongelang(ir: Vec<SpongeIR>) -> String {
+    let mut out = String::new();
+
+    for node in ir {
+        match node {
+            SpongeIR::Block(name) => {
+                out.push_str(&format!("[sponge {}]\n", name));
+            }
+            SpongeIR::Echo(msg) => {
+                out.push_str(&format!("  ~echo \"{}\"\n", msg));
+            }
+            SpongeIR::Dissolve => {
+                out.push_str("  ~dissolve\n[/sponge]\n");
             }
         }
     }
