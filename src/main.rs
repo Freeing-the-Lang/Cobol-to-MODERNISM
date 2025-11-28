058bda4a77adf56e225dd80ec59286b8cc2a2ed2
한진희 <0200134hjh@gmail.com>
2025-11-28 16:48:38 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 mod lexer;
 mod parser;
 mod ast;
+mod modernism_ir;
+mod spongelang_ir;
+mod spongelang_gen;
 mod transpiler;
 
 use std::fs;
 
 fn main() {
-    let file_path = "examples/hello.cobol";
-    let code = fs::read_to_string(file_path)
-        .expect("Cannot read COBOL file.");
+    let code = fs::read_to_string("examples/hello.cobol").unwrap();
 
-    println!("=== COBOL INPUT ===");
-    println!("{code}");
-
+    // 1) COBOL → AST
     let tokens = lexer::tokenize(&code);
     let ast = parser::parse(tokens);
-    let modern = transpiler::to_modernism(ast);
 
-    println!("\n=== MODERN OUTPUT ===");
-    println!("{modern}");
+    // 2) AST → Modernism IR
+    let modern = transpiler::to_modernism_ir(ast);
+
+    // 3) Modernism IR → Spongelang IR
+    let sponge_ir = spongelang_gen::modernism_to_spongelang(modern);
+
+    // 4) Spongelang 코드 생성
+    let sponge_code = spongelang_gen::generate_spongelang(sponge_ir);
+
+    println!("=== Spongelang OUTPUT ===");
+    println!("{sponge_code}");
 }
